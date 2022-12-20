5d0fe8548328d2784fc6e2c5e0ef48ce55db210d
hakanhaciogluuu <hknhcgl@gmail.com>
2022-12-21 00:24:26 +0300

    Kullanıcı giriş çıkış işlemleri gerçekleştirildi
    .

--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 from . import views
 
-# http://127.0.0.1:8000/
-# http://127.0.0.1:8000/index
-# http://127.0.0.1:8000/blogs
-# http://127.0.0.1:8000/blogs/2
-
 
 
 urlpatterns = [
     path("", views.index, name= "home"),
     path("category/<slug:slug>", views.urunler, name= "urunler"),
+    #####account
+    path("login", views.login_request, name= "login"),
+    path("register", views.register_request, name= "register"),
+    path("logout", views.logout_request, name= "logout"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,73 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from main.models import Category, Urun
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+
+#hesap işlemleri
+
+def login_request(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+    context = {
+        "categories" : Category.objects.all()
+    }
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            return render(request, "login.html", {
+                "error": "username ya da password yanlis" 
+            })
+    return render(request, "login.html", context)
+
+def register_request(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+    context = {
+        "categories" : Category.objects.all()
+    }
+    if request.method == 'POST':
+        username = request.POST["username"]
+        email = request.POST["email"]
+        firstname = request.POST["firstname"]
+        lastname = request.POST["lastname"]
+        password = request.POST["password"]
+        repassword = request.POST["repassword"]
+
+        if password == repassword:
+            if User.objects.filter(username=username).exists():
+                return render(request, "register.html", {"error": "username kullanılıyor."})
+            else:
+                if User.objects.filter(email=email).exists():
+                    return render(request, "register.html", {"error": "email kullanılıyor."})
+                else:
+                    user = User.objects.create_user(username=username, email=email,first_name=firstname, last_name=lastname,password=password)
+                    user.save()
+                    return redirect("login")
+        else:
+            return render(request, "register.html", {"error":"parolar eşleşmiyor."})
+    return render(request, "register.html",context)
+
+def logout_request(request):
+    logout(request)
+    return redirect("home")
+
+
+
+
+
+
+
+
 
 
 def index(request):
